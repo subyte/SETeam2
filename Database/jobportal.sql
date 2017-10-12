c0e8e1a0d011477d00b3a2e19ab64e14067d8379
saurinkhedia <32724558+saurinkhedia@users.noreply.github.com>
2017-10-11 23:17:22 -0400

    Add files via upload

---
+++ Database/jobportal.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE  IF NOT EXISTS `jobportal`
+USE `jobportal`;
+
+DROP TABLE IF EXISTS `employer`;
+
+CREATE TABLE `employer` (
+  `empid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `cmpname` varchar(45) NOT NULL,
+  `contactname` varchar(45) NOT NULL,
+  `contactno` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `state` varchar(45) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  PRIMARY KEY (`empid`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `jobseeker`;
+
+CREATE TABLE `jobseeker` (
+  `jskid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `contactno` varchar(45) NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  `dob` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `education` varchar(45) NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `state` varchar(45) NOT NULL,
+  `workexp` varchar(45) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `resumefile` varchar(45) NOT NULL,
+  PRIMARY KEY (`jskid`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `type` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
