c469d266e282e1533daef8a649b29477378c5597
saurinkhedia <32724558+saurinkhedia@users.noreply.github.com>
2017-11-02 00:40:33 -0400

    Add files via upload

--- Database/jobportal.sql
+++ Database/jobportal.sql
@@ -1,5 +1,17 @@
-CREATE DATABASE  IF NOT EXISTS `jobportal`
-USE `jobportal`;
+CREATE DATABASE  IF NOT EXISTS `jobportal` 
+
+DROP TABLE IF EXISTS `adminuser`;
+
+CREATE TABLE `adminuser` (
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `adminuser` WRITE;
+
+INSERT INTO `adminuser` VALUES ('admin','admin');
+
+UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `employer`;
 
@@ -16,6 +28,10 @@ CREATE TABLE `employer` (
   PRIMARY KEY (`empid`),
   UNIQUE KEY `username_UNIQUE` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `employer` WRITE;
+
+UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `jobseeker`;
 
@@ -36,7 +52,11 @@ CREATE TABLE `jobseeker` (
   `resumefile` varchar(45) NOT NULL,
   PRIMARY KEY (`jskid`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `jobseeker` WRITE;
+
+UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `login`;
 
@@ -44,5 +64,10 @@ CREATE TABLE `login` (
   `userid` int(11) NOT NULL,
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-  `type` varchar(45) NOT NULL
+  `type` varchar(45) NOT NULL,
+  `authorization` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `login` WRITE;
+
+UNLOCK TABLES;
