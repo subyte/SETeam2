617904ed2dc62f0e1120717cd1c80e8dccfa0daa
saurinkhedia <32724558+saurinkhedia@users.noreply.github.com>
2017-11-15 17:38:18 -0500

    Add files via upload

--- Database/jobportal.sql
+++ Database/jobportal.sql
@@ -13,6 +13,22 @@ INSERT INTO `adminuser` VALUES ('admin','admin');
 
 UNLOCK TABLES;
 
+DROP TABLE IF EXISTS `appliedjobs`;
+
+CREATE TABLE `appliedjobs` (
+  `ajid` int(11) NOT NULL AUTO_INCREMENT,
+  `jobid` varchar(45) NOT NULL,
+  `companyname` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  PRIMARY KEY (`ajid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `appliedjobs` WRITE;
+
+UNLOCK TABLES;
+
 DROP TABLE IF EXISTS `employer`;
 
 CREATE TABLE `employer` (
@@ -27,9 +43,31 @@ CREATE TABLE `employer` (
   `category` varchar(100) NOT NULL,
   PRIMARY KEY (`empid`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `employer` WRITE;
+
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `jobs`;
+
+CREATE TABLE `jobs` (
+  `jid` int(11) NOT NULL AUTO_INCREMENT,
+  `jobid` varchar(45) NOT NULL,
+  `companyname` varchar(45) NOT NULL,
+  `contactname` varchar(45) NOT NULL,
+  `jobtitle` varchar(45) NOT NULL,
+  `workexp` varchar(45) NOT NULL,
+  `education` varchar(45) NOT NULL,
+  `jobcategory` varchar(100) NOT NULL,
+  `jobskills` varchar(150) CHARACTER SET big5 NOT NULL,
+  `jobdescription` varchar(400) NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `state` varchar(45) NOT NULL,
+  PRIMARY KEY (`jid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `jobs` WRITE;
 
 UNLOCK TABLES;
 
@@ -42,7 +80,7 @@ CREATE TABLE `jobseeker` (
   `lastname` varchar(45) NOT NULL,
   `contactno` varchar(45) NOT NULL,
   `gender` varchar(45) NOT NULL,
-  `dob` varchar(45) NOT NULL,
+  `dob` date NOT NULL,
   `email` varchar(45) NOT NULL,
   `education` varchar(45) NOT NULL,
   `city` varchar(45) NOT NULL,
@@ -52,7 +90,7 @@ CREATE TABLE `jobseeker` (
   `resumefile` varchar(45) NOT NULL,
   PRIMARY KEY (`jskid`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `jobseeker` WRITE;
 
